2e77a0b3f78df2df05ac722d83722ca91ae69bfb
Avinash-choubey-4719 <ac4719@srmist.edu.in>
2022-08-17 18:52:34 +0530

    Initial commit

---
+++ app/src/main/java/com/avinash/whatsapplite/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.avinash.whatsapplite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
